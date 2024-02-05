92e59d80a39459cf343815bd6dceebaf4d6a063f
Shehna Jaleel <shehnajaleel269@gmail.com>
2024-02-05 10:23:13 +0530

    Firebase configured

---
+++ src/firebase.js
@@ -0,0 +1,27 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDMp_sxNegvJwmuSzQs3wtvp5a3tCpTr0Q",
+  authDomain: "weather-ba52f.firebaseapp.com",
+  projectId: "weather-ba52f",
+  storageBucket: "weather-ba52f.appspot.com",
+  messagingSenderId: "7021025015",
+  appId: "1:7021025015:web:1797adc3d1f2642c3fa5f2",
+  measurementId: "G-897MDRNGJ5"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+export default firebase;
